899fbc27f13b466814d36e3870297f7acee637c6
ayush-ranjan07 <ayush.ranjan0503@gmail.com>
2023-10-31 10:36:39 +0530

    Added the testimonial

--- main.js
+++ main.js
@@ -7,10 +7,19 @@ window.addEventListener('scroll',()=>{
 
 // Show hide faq answer
 
-const faqs=document.querySelectorAll('.faqs');
+const faqs=document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
     faq.addEventListener('click',() => {
         faq.classList.toggle('open');
+
+        //change icon
+        const icon =faq.querySelector('.faq__icon i');
+        if(icon.className==='uil uil-plus'){
+            icon.className = "uil uil-minus";
+        }
+        else{
+            icon.className= "uil uil-plus";
+        }
     })
 })
